43fa35a1eefcbe975861e852dc4884977a4e45d3
Allen Lam <allenlam465@gmail.com>
2018-10-29 08:45:46 -0700

    Initail commit. Created Board and its print method.

---
+++ src/Board.java
@@ -0,0 +1,39 @@
+public class Board {
+	
+	private char[][] board;
+	
+	public Board() {
+		this.board = new char[8][8];
+		
+		for(int i = 0; i < 8; i++) {
+			for(int j = 0; j < 8; j++){
+				board[i][j] = '-';
+			}
+		}
+	}
+	
+	public void setBoard(char[][] board) {
+		this.board = board;
+	}
+	
+	public char[][] getBoard() {
+		return board;
+	}
+	
+	public String printBoard() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("  1 2 3 4 5 6 7 8 	Player vs. Oppenent");
+		
+		for(int i = 0; i < 8; i ++) {
+			sb.append("\n" + Character.toString(((char)(65 + i))) + " ");
+			for(int j = 0; j < 8; j++) {
+				sb.append(board[i][j] + " ");
+			}
+			sb.append("	   " + "Moves Here");	
+		}
+		
+		return sb.toString();
+	}
+	
+}
---
+++ src/Driver.java
@@ -0,0 +1,10 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Board x = new Board();
+		
+		System.out.println(x.printBoard());
+	}
+
+}
