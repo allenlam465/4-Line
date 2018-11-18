fa161275836e0339892ec6de52f1df23996ac365
Nathan <master555x@gmail.com>
2018-11-17 19:41:55 -0800

    Driver Update

--- src/Driver.java
+++ src/Driver.java
@@ -12,11 +12,15 @@ public class Driver {
 		Board game = new Board();
 		Scanner s = new Scanner(System.in);
 		boolean matchOver = false;
+                
+                double alpha = Double.NEGATIVE_INFINITY;
+                double beta  = Double.POSITIVE_INFINITY;
 
-		System.out.println("4 In a Line Game with Minmax and Alpha-Beta Pruning");
-		System.out.print("Time allowed for generating moves (seconds)?\n>");
+		//System.out.println("4 In a Line Game with Minmax and Alpha-Beta Pruning");
+		//System.out.print("Time allowed for generating moves (seconds)?\n>");
 
-		int time = Integer.parseInt(s.nextLine());
+		//int time = Integer.parseInt(s.nextLine());
+                int time = 60;
 
 		do{
 			System.out.print("\nStarting player: \n1. Player\n2. Opponent \n>");
@@ -25,12 +29,12 @@ public class Driver {
 
 			if(input.equals("1")) {
 				currentPlayer = 1;
-				System.out.println("Player goes first.");
+				System.out.println("\nPlayer goes first.");
 				break;
 			}
 			else if(input.equals("2")) {
 				currentPlayer = 2;
-				System.out.println("Opponents goes first.");
+				System.out.println("\nOpponents goes first.");
 				break;
 			}
 			else
@@ -38,12 +42,13 @@ public class Driver {
 
 		}while(true);
 
-		while(!game.checkWin('X') && !game.checkWin('O')) {
+		while(!game.checkWin('X') && !game.checkWin('O') && !game.checkDraw()) {
 
 			System.out.println(game.printBoard());
+                        //ABP(game, 1, 1, 1, 1);
 
 			do{
-				System.out.print("Input Move \n>");
+				System.out.print("\nInput Move \n>");
 				input = s.nextLine();
 
 				if(game.validateMove(input)) {
@@ -65,11 +70,64 @@ public class Driver {
 		s.close();
 	}
         
-        static int miniMaxAlphaBetaPruning(Board state, int currentPlayer, int depth) {
+        static double ABP(Board game, int currentPlayer, int alpha, int beta, int depth) {
+            Board tempGame = new Board();
+            tempGame = game;
             
+            System.out.println("\n" + tempGame.printBoard());
             
+            double v = MaxValue(tempGame, alpha, beta, depth);
             
-            return 0;
+            return v; //return action
+        }
+        
+        
+        static double MaxValue(Board state, int alpha, int beta, int depth) {
+            double utility = 0;
+            
+            if(state.checkDraw()) {
+                return 1;
+            }
+            else if(state.checkWin('O')) {
+                return Double.POSITIVE_INFINITY;
+            }
+            else if(state.checkWin('X')){
+                return Double.NEGATIVE_INFINITY;
+            }
+            else {
+                utility += state.evaluateBoard();
+            }
+            
+            double v = Double.NEGATIVE_INFINITY;
+            
+            //go through all actions and update v, a, or accordingly
+            //call MaxValue again
+            
+            return v;
+        }
+        
+        static double MinValue(Board state, int alpha, int beta) {
+            double utility = 0;
+            
+            if(state.checkDraw()) {
+                return 1;
+            }
+            else if(state.checkWin('O')) {
+                return Double.POSITIVE_INFINITY;
+            }
+            else if(state.checkWin('X')){
+                return Double.NEGATIVE_INFINITY;
+            }
+            else {
+                utility += state.evaluateBoard();
+            }
+            
+            double v = Double.POSITIVE_INFINITY;
+            
+            //go through all actions and update v, a, or accordingly
+            //call MinValue again
+            
+            return v;
         }
         
 
